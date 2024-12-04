0afa8fe1c445ac3c7b47f6d61abcde606f74b305
d4nny815 <d4nny815@gmail.com>
2024-12-04 01:41:42 -0800

    harddisk

--- minls.c
+++ minls.c
@@ -1,5 +1,8 @@
 #include "min_common.h"
 
+void print_file();
+void print_dir();
+
 int main(int argc, char** argv) {
     MinArgs_t args;
     SuperBlock_t super_block;
@@ -22,5 +25,34 @@ int main(int argc, char** argv) {
     fseek(args.image_file, root_inode_addr, SEEK_SET);
     bytes = fread(&root_inode, sizeof(Inode_t), 1, args.image_file);
 
+    print_file(root_inode);
+
+
     return 0;
+}
+
+void print_perms(uint16_t mode) {
+    // Print permissions
+    printf((mode & DIRECTORY) ? "d" : "-");
+    printf((mode & OWNER_READ) ? "r" : "-");
+    printf((mode & OWNER_WRITE) ? "w" : "-");
+    printf((mode & OWNER_EXEC) ? "x" : "-");
+    printf((mode & GROUP_READ) ? "r" : "-");
+    printf((mode & GROUP_WRITE) ? "w" : "-");
+    printf((mode & GROUP_EXEC) ? "x" : "-");
+    printf((mode & OTHER_READ) ? "r" : "-");
+    printf((mode & OTHER_WRITE) ? "w" : "-");
+    printf((mode & OTHER_EXEC) ? "x" : "-");
+    printf("\n");
+}
+
+void print_dir() {
+
+}
+
+void print_file(Inode_t inode) {
+    printf("%o\n", inode.mode);
+    print_perms(inode.mode);
+    printf("%s");
+
 }
