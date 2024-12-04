6993bcac29759f1b6003176effbe1ad7b0f594ad
d4nny815 <d4nny815@gmail.com>
2024-12-04 02:33:48 -0800

    reading root dir

--- minls.c
+++ minls.c
@@ -4,6 +4,7 @@ void print_file();
 void print_dir();
 
 Inode_t* inode_list;
+size_t cur_inode_ind;
 
 int main(int argc, char** argv) {
     MinArgs_t args;
@@ -26,19 +27,20 @@ int main(int argc, char** argv) {
     size_t root_inode_addr = (partition_addr) +
         (2 + super_block.i_blocks + super_block.z_blocks) * super_block.blocksize;
 
-    inode_list = (Inode_t*) root_inode_addr;
+    inode_list = (Inode_t*) malloc(sizeof(Inode_t) * super_block.ninodes);
     
     fseek(args.image_file, root_inode_addr, SEEK_SET);
-    bytes = fread(&root_inode, sizeof(Inode_t), 1, args.image_file);
+    bytes = fread(inode_list, sizeof(Inode_t), super_block.ninodes, args.image_file);
 
     size_t zone_size = super_block.blocksize << super_block.log_zone_size;
 
     DirEntry_t* dir_entry = (DirEntry_t*) malloc(zone_size);
-    fseek(args.image_file, partition_addr + (zone_size * root_inode.zone[0]), SEEK_SET);
+    fseek(args.image_file, partition_addr + (zone_size * inode_list[0].zone[0]), SEEK_SET);
     fread(dir_entry, zone_size, 1, args.image_file);
 
-    print_dir(root_inode, dir_entry);
+    print_dir(inode_list[0], dir_entry);
 
+    printf("done\n");
     return 0;
 }
 
@@ -57,9 +59,11 @@ void print_perms(uint16_t mode) {
 }
 
 void print_dir(Inode_t inode, DirEntry_t* dir_entry) {
-    for(int i = 0; i < inode.links + 2; i++) {
-        print_perms(inode.mode);
-        printf("%10d %s\n", inode.size, dir_entry->name);
+    for(int i = 0; i < inode.size / sizeof(DirEntry_t); i++) {
+        // print_perms(inode.mode);
+        print_perms(inode_list[dir_entry->inode - 1].mode);
+
+        printf("%10d %s %u\n", inode_list[dir_entry->inode - 1].size, dir_entry->name, dir_entry->inode);
         dir_entry++;
     }
 
